c34ee5c52629eb7ab83a7b4f9460895105016c2b
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2024-11-28 22:12:12 +0400

    commit

---
+++ goa homeworks/Day 10/homework/homework.py
@@ -0,0 +1,328 @@
+num= str(10)
+num1= str(20)
+num2= str(15)
+num3= str(25)
+num4= str(9)
+num5=str(11)
+num6=str(23)
+num7=str(24)
+num8=str(26)
+num9= str(90)
+num10= str(19)
+print(num + num1)
+print(num2 + num3)
+print(num4 + num5)
+print(num7 + num8)
+print(num5 + num5)
+print(num9 + num6)
+print(num5 + num3)
+print(num + num2)
+print(num7 + num2)
+print(num10 + num3)
+
+number= float(10)
+number1= float(20)
+number2= float(15)
+number3= float(25)
+number4= float(9)
+number5=float(11)
+number6=float(23)
+number7=float(24)
+number8=float(26)
+number9= float(90)
+number10= float(19)
+print(number + number1)
+print(number2 + number3)
+print(number4 + number5)
+print(number7 + number)
+print(number5 + number5)
+print(number9 + number6)
+print(number5 + number3)
+print(number + number2)
+print(number7 + number2)
+print(number10 + number3)
+
+numb= int(10)
+numb1= int(20)
+numb2= int(15)
+numb3= int(25)
+numb4= int(9)
+numb5=int(11)
+numb6=int(23)
+numb7=int(24)
+numb8=int(26)
+numb9= int(90)
+numb10= int(19)
+print(numb + numb1)
+print(numb2 + numb3)
+print(numb4 + numb5)
+print(numb7 + numb8)
+print(numb5 + numb5)
+print(numb9 + numb6)
+print(numb5 + numb3)
+print(numb + numb2)
+print(numb7 + numb2)
+print(numb10 + numb3)
+# end of second homework
+# start of 5th homework
+
+x = 10
+x1 = 18
+x2 = 12
+x3 = 14
+x4 = 16
+x5 = 11
+x6 = 12
+x7 = 15
+x8 = 14
+x9 = 13
+x10 = 80
+x11 = 70
+x12 = 60
+x13 = 50
+x14 = 40
+x15 = 30
+x16 = 20
+x17 = 23
+x18 = 22
+x19 = 21
+
+print(x>x1)
+print(x2>x3)
+print(x4>x5)
+print(x6>x7)
+print(x8>x9)
+print(x10>x11)
+print(x12>x13)
+print(x14>x15)
+print(x16>x17)
+print(x18>x19)
+
+
+y = 10
+y1 = 18
+y2 = 12
+y3 = 14
+y4 = 16
+y5 = 11
+y6 = 12
+y7 = 15
+y8 = 14
+y9 = 13
+y10 = 80
+y11 = 70
+y12 = 60
+y13 = 50
+y14 = 40
+y15 = 30
+y16 = 20
+y17 = 23
+y18 = 22
+y19 = 21
+
+print(y<y1)
+print(y2<y3)
+print(y4<y5)
+print(y6<y7)
+print(y8<y9)
+print(y10<y11)
+print(y12<y13)
+print(y14<y15)
+print(y16<y17)
+print(y18<y19)
+
+z = 10
+z1 = 18
+z2 = 12
+z3 = 14
+z4 = 16
+z5 = 11
+z6 = 12
+z7 = 15
+z8 = 14
+z9 = 13
+z10 = 80
+z11 = 70
+z12 = 60
+z13 = 50
+z14 = 40
+z15 = 30
+z16 = 20
+z17 = 23
+z18 = 22
+z19 = 21
+print(z<=z1)
+print(z2<=z3)
+print(z4<=z5)
+print(z6<=z7)
+print(z8<=z9)
+print(z10<=z11)
+print(z12<=z13)
+print(z14<=z15)
+print(z16<=z17)
+print(z18<=z19)
+
+v = 10
+v1 = 18
+v2 = 12
+v3 = 14
+v4 = 16
+v5 = 11
+v6 = 12
+v7 = 15
+v8 = 14
+v9 = 13
+v10 = 80
+v11 = 70
+v12 = 60
+v13 = 50
+v14 = 40
+v15 = 30
+v16 = 20
+v17 = 23
+v18 = 22
+v19 = 21
+
+print(v>=v1)
+print(v2>=v3)
+print(v4>=v5)
+print(v6>=v7)
+print(v8>=v9)
+print(v10>=v11)
+print(v12>=v13)
+print(v14>=v15)
+print(v16>=v17)
+print(v18>=v19)
+
+c = 10
+c1 = 18
+c2 = 12
+c3 = 14
+c4 = 16
+c5 = 11
+c6 = 12
+c7 = 15
+c8 = 14
+c9 = 13
+c10 = 80
+c11 = 70
+c12 = 60
+c13 = 50
+c14 = 40
+c15 = 30
+c16 = 20
+c17 = 23
+c18 = 22
+c19 = 21
+
+print(c==c1)
+print(c2==c3)
+print(c4==c5)
+print(c6==c7)
+print(c8==c9)
+print(c10==c11)
+print(c12==c13)
+print(c14==c15)
+print(c16==c17)
+print(c18==c19)
+
+s = 10
+s1 = 18
+s2 = 12
+s3 = 14
+s4 = 16
+s5 = 11
+s6 = 12
+s7 = 15
+s8 = 14
+s9 = 13
+s10 = 80
+s11 = 70
+s12 = 60
+s13 = 50
+s14 = 40
+s15 = 30
+s16 = 20
+s17 = 23
+s18 = 22
+s19 = 21
+
+print(s!=s1)
+print(s2!=s3)
+print(s4!=s5)
+print(s6!=s7)
+print(s8!=s9)
+print(s10!=s11)
+print(s12!=s13)
+print(s14!=s15)
+print(s16!=s17)
+print(s18!=s19)
+
+
+# start of 6th homework
+
+age = int(input("enter your age :  "))
+after = age + 10
+print(after)
+# end of 6th homework
+
+# start of 7th homework
+age1= int(input(" enter your age here :  ") )
+age2= int(input(" enter your age here :  ") )
+age3= int(input(" enter your age here :  ") )
+
+average = (age1 + age2 + age3) / 3
+print(average)
+# end of 7th homework
+
+# start of 8th homework
+
+firstnum = float( input(" enter number here : " ))
+secondnum = float( input(" enter number here : " ))
+addition = firstnum + secondnum
+deduction = firstnum - secondnum
+division = firstnum / secondnum
+multiplication= firstnum * secondnum
+power = firstnum ** secondnum
+power2= secondnum ** firstnum
+print(addition)
+print(deduction)
+print(division)
+print(multiplication)
+print(power)
+print(power2)
+# end of 8th homework
+
+
+#start of 9th homework
+
+float1 =  float(input(" enter any number here :  "))
+float2 =  float(input(" enter any number here :  "))
+float3 =  float(input(" enter any number here :  "))
+
+mimateba = float1 + float2 + float3
+gamokleba = float1 - float2 - float3
+gayofa = float1 / float2 / float3
+gamravleba = float1 * float2 * float3
+print(mimateba)
+print(gamokleba)
+print(gayofa)
+print(gamravleba)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ goa homeworks/Day 16/classwork/classwork.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter num"))
+range1= (num1)
+for i in range1:
+    print(range1 + i)
