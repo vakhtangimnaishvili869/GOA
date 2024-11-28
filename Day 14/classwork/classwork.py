452885a6f57a34c0e052cb6b27fc76fe4b3a2ce4
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2024-11-29 00:03:34 +0400

    commit

---
+++ Day 14/classwork/classwork.py
@@ -0,0 +1,12 @@
+# for i in range(20):
+#     print("vato imnaishvili")
+
+# num1, num2 = int(input("enter number : ")),num2 = int(input("enter number : "))
+# for i in range(num1, num2):
+#     print(i **2  )
+#     print(i ** 2 )
+
+n1 = int(input("enter any num here:  " ))
+
+for x in range (n1):
+    print(x **2)
