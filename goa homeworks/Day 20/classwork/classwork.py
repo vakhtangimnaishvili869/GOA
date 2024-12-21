bd86580ceba936db39882e0bcec7395ea878d7aa
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2024-12-21 20:26:11 +0400

    commit

---
+++ goa homeworks/Day 20/classwork/classwork.py
@@ -0,0 +1,17 @@
+list1= [1, 2, 3 ,4, 5 ,6, 7 ,8 ,9, 10]
+num1 = list1[0]
+num2 = list1[1]
+num3 = list1[2]
+num4 = list1[3]
+num5 = list1[4]
+num6 = list1[5]
+num7 = list1[6]
+num8 = list1[7]
+num9 = list1[8]
+num10 = list1[9]
+
+number1 =  int (input("enter num"))
+number2 =  int (input("enter num"))
+number3 =  int (input("enter num"))
+list2 = [number1 , number2 , number3]
+print(list2)
