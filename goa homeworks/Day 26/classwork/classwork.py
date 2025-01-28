541b37020d6889220af4b95b27c58f683de63315
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2025-01-29 00:33:03 +0400

    commit

---
+++ goa homeworks/Day 26/classwork/classwork.py
@@ -0,0 +1,10 @@
+#1)
+def multiply(a, b):
+    return a * b
+
+#2)
+def even_or_odd(number):
+    if number %2 == 0:
+        return "Even"
+    else:
+        return "Odd"
---
+++ goa homeworks/Day 26/homework/homework.py
@@ -0,0 +1,23 @@
+#1)
+def multiply(a, b):
+    return a * b
+#2)
+
+
+def even_or_odd(number):
+    if number %2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+#3)
+def number_to_string(num):
+    return str(num)
+#4)
+def solution(string):
+    return string[::-1]
+#5)
+def opposite(number):
+    return -number
+
+
+
