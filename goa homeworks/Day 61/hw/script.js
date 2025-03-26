0a83cce82d985932a87cc518b7c8fdf0375e0665
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2025-03-27 02:04:40 +0400

    commit

---
+++ goa homeworks/Day 61/hw/script.js
@@ -0,0 +1,63 @@
+let age = confirm("Are you an adult?");
+        
+if (age) {
+    alert("You are an adult");
+} 
+else {
+    alert("You are an undarage");
+}
+
+
+console.log(45 == 10);
+console.log("jupiter" == "earth");
+console.log(11 == 11);
+console.log( 40 == "40");
+console.log(60 == 10);
+
+
+console.log(10 === 10);
+console.log('mmath' === 'marh');
+console.log(' goa' === 'goam');
+console.log(2 === 2);
+console.log(10 === 20);
+
+
+
+
+console.log(10 != 20);
+console.log(1 != 23);
+console.log(43 != 43);
+console.log(12 != 50);
+console.log(10 != 10);
+
+
+
+
+
+
+console.log(10 > 5);     
+console.log(9 > 8);      
+console.log(51 > 50);   
+console.log(7 > 2);      
+console.log(0 > -3);    
+
+
+console.log(8 < 3);     
+console.log(3 < 4);      
+console.log(120 < 20);   
+console.log(1 < 7);      
+console.log(-3 < -2);   
+
+
+console.log(15 >= 5);    
+console.log(2 >= 8);     
+console.log(180 >= 100); 
+console.log(7 >= 6);     
+console.log(-1 >= -1);   
+
+
+console.log(8 <= 6);    
+console.log(3 <= 4);     
+console.log(20 <= 20); 
+console.log(7 <= 7);     
+console.log(-10 <= -3);  
