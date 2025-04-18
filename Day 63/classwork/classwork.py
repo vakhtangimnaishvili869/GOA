a412dc04779beb4d57f5f3e1a343b4fd63850837
vakhtangimnaishvili869 <imnaishvilivato18@gmail.com>
2025-04-18 04:27:25 +0400

    Delete Day 63 directory

--- Day 63/classwork/classwork.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter your age: "))
-
-if age < 13:
-    print("You are a kid")
-elif age >= 13 and age <= 17:
-    print("You are not adult yet")
-else:
-    print("You are an adult")
